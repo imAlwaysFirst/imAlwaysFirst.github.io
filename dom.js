ab3dda04e2f62b9da4bdabb6fe393b9c0e1bf9e3
Steven Lasso <Lasso.steven08@gmail.com>
2024-08-12 15:07:45 +0000

    Not finished

--- dom.js
+++ dom.js
@@ -1,10 +1,10 @@
-//select the button element
+//select the button element \\
+document.querySelector("body").style.cssText = "color: white; background-color: white;"
 let myButton = document.querySelector("button")
 
-//declared event handler function 
-function handleClick(){
-    alert('button was clicked')
-    document.querySelector("body").style.cssText = "color: white; background-color: black;"
+//declared event handler function \\
+function handleClick() {
+    document.querySelector("body").style.cssText = "color: White; background-color: Black;"
 }
 
 //use addEventListener to run the handleClick function when the button is clicked
